ee27ba771ade7dfe8d06deab201323d7ec2a66bc
Chad <chadjcampbell@gmail.com>
2022-10-22 14:56:32 -0400

    board cell divs set up

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import './style.scss'
 import { Gameboard } from './gameboard'
 import { Player } from './player'
 import { Ship } from './ship'
+
+const playerBoard = new Gameboard()
+const playerDisplay = document.querySelector('#playerdisplay')
+
+const computerBoard = new Gameboard()
+const computerDisplay = document.querySelector('#computerdisplay')
+
+playerBoard.gameBoard.forEach((cell) => {
+  const cellDiv = document.createElement('div')
+  cellDiv.classList.add('cell')
+  playerDisplay.appendChild(cellDiv)
+})
+
+computerBoard.gameBoard.forEach((cell) => {
+  const cellDiv = document.createElement('div')
+  cellDiv.classList.add('cell')
+  computerDisplay.appendChild(cellDiv)
+})

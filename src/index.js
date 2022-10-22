31bf7c782eca77925b1727a9ca1361b504b15a88
Chad <chadjcampbell@gmail.com>
2022-10-22 16:23:10 -0400

    start event listeners

--- src/index.js
+++ src/index.js
@@ -9,15 +9,19 @@ const playerDisplay = document.querySelector('#playerdisplay')
 const computerBoard = new Gameboard()
 const computerDisplay = document.querySelector('#computerdisplay')
 
-playerBoard.gameBoard.forEach((cell) => {
-  const cellDiv = document.createElement('div')
-  cellDiv.classList.add('cell')
-  cellDiv.dataset.x = cell.x
-  cellDiv.dataset.y = cell.y
-  cellDiv.dataset.occupied = cell.occupied
-  cellDiv.dataset.beenHit = cell.beenHit
-  playerDisplay.appendChild(cellDiv)
-})
+function renderPlayerBoard() {
+  playerBoard.gameBoard.forEach((cell) => {
+    const cellDiv = document.createElement('div')
+    cellDiv.classList.add('cell')
+    cellDiv.dataset.x = cell.x
+    cellDiv.dataset.y = cell.y
+    cellDiv.dataset.occupied = cell.occupied
+    cellDiv.dataset.beenHit = cell.beenHit
+    playerDisplay.appendChild(cellDiv)
+  })
+}
+
+renderPlayerBoard()
 
 computerBoard.gameBoard.forEach((cell) => {
   const cellDiv = document.createElement('div')
@@ -28,3 +32,19 @@ computerBoard.gameBoard.forEach((cell) => {
   cellDiv.dataset.beenHit = cell.beenHit
   computerDisplay.appendChild(cellDiv)
 })
+
+const playerBoardCells = document.querySelectorAll('.cell')
+
+playerBoardCells.forEach((cellDiv) => {
+  cellDiv.addEventListener('click', () => {
+    playerBoard.receiveAttack([
+      Number(cellDiv.dataset.x),
+      Number(cellDiv.dataset.y),
+    ])
+    updateCell(cellDiv)
+  })
+})
+
+function updateCell(cellDiv) {
+  cellDiv.dataset.beenHit = true
+}

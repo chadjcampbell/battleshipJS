7bbc5e890b9f4b7c50e692012b9ef43d420b86c1
Chad <chadjcampbell@gmail.com>
2022-10-24 17:26:33 -0400

    set up gameloop and add reset button

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import './style.scss'
 import { Gameboard } from './gameboard'
 import { Player } from './player'
-import { Ship } from './ship'
 
 const playerBoard = new Gameboard()
 const playerDisplay = document.querySelector('#playerdisplay')
 
 const computerBoard = new Gameboard()
 const computerDisplay = document.querySelector('#computerdisplay')
+const computerAI = new Player()
 
 playerBoard.randomPlacement()
 computerBoard.randomPlacement()
 
 function renderPlayerBoard() {
+  playerDisplay.innerHTML = ''
   playerBoard.gameBoard.forEach((cell) => {
     const cellDiv = document.createElement('div')
     cellDiv.classList.add('playerCell')
@@ -27,6 +28,7 @@ function renderPlayerBoard() {
 renderPlayerBoard()
 
 function renderComputerBoard() {
+  computerDisplay.innerHTML = ''
   computerBoard.gameBoard.forEach((cell) => {
     const cellDiv = document.createElement('div')
     cellDiv.classList.add('computerCell')
@@ -40,30 +42,32 @@ function renderComputerBoard() {
 
 renderComputerBoard()
 
-const playerBoardCells = document.querySelectorAll('.playerCell')
-
-playerBoardCells.forEach((cellDiv) => {
-  cellDiv.addEventListener('click', () => {
-    playerBoard.receiveAttack([
-      Number(cellDiv.dataset.x),
-      Number(cellDiv.dataset.y),
-    ])
-    updateCell(cellDiv)
+function playerTurn() {
+  const computerBoardCells = document.querySelectorAll('.computerCell')
+  computerBoardCells.forEach((cellDiv) => {
+    cellDiv.addEventListener('click', () => {
+      computerBoard.receiveAttack([
+        Number(cellDiv.dataset.x),
+        Number(cellDiv.dataset.y),
+      ])
+      renderComputerBoard()
+      computerAI.randomAttack(playerBoard)
+      renderPlayerBoard()
+      gameLoop()
+    })
   })
-})
-
-const computerBoardCells = document.querySelectorAll('.computerCell')
-
-computerBoardCells.forEach((cellDiv) => {
-  cellDiv.addEventListener('click', () => {
-    computerBoard.receiveAttack([
-      Number(cellDiv.dataset.x),
-      Number(cellDiv.dataset.y),
-    ])
-    updateCell(cellDiv)
-  })
-})
-
-function updateCell(cellDiv) {
-  cellDiv.dataset.beenHit = true
 }
+
+//main game loop
+
+function gameLoop() {
+  if (!playerBoard.fleetSunk() && !computerBoard.fleetSunk()) {
+    playerTurn()
+  } else if (playerBoard.fleetSunk()) {
+    alert('You Lose!')
+  } else if (computerBoard.fleetSunk()) {
+    alert('You Win!')
+  }
+}
+
+playerTurn()
